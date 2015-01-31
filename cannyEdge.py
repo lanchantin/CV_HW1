997a57034167f0f02129c60f79bfe6f7890f7cf6
Jack Lanchantin <jacklanchantin1@gmail.com>
2015-01-31 01:36:31 -0500

    finished pt one (kinda)

--- cannyEdge.py
+++ cannyEdge.py
@@ -23,11 +23,11 @@ print(img.shape)
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.299, 0.587, 0.144])
 
-def gaussian(x, mu, sigma):
-  return exp( -(((x-mu)/(sigma))**2)/2.0 )
-
 g = rgb2gray(img)
 
+
+def gaussian(x, mu, sigma):
+  return exp( -(((x-mu)/(sigma))**2)/2.0 )
 kernel_radius = 3 # for an 7x7 filter
 sigma = 10 # for [-2*sigma, 2*sigma]
 hkernel = [gaussian(x, kernel_radius, sigma) for x in range(2*kernel_radius+1)]
@@ -41,51 +41,85 @@ k = np.array(kernel2d)
 blurImg = scipy.signal.convolve2d(g, k)
 #plt.imshow(blurImg, cmap = plt.get_cmap('gray'));plt.show()
 
-
+#Sobel filter values
 Kgx = np.array([[ -1, 0, 1], [-2,0,2], [-1,0,1]])
-
 Kgy = np.array([[1,2,1], [0,0,0], [-1,-2,-1]])
 
-Fx = scipy.signal.convolve2d(blurImg, Kgx)
 
+Fx = scipy.signal.convolve2d(blurImg, Kgx)
 Fy = scipy.signal.convolve2d(blurImg, Kgy)
 
 #plt.imshow(Fx, cmap = plt.get_cmap('gray')); plt.show()
-
 #plt.imshow(dx, cmap = plt.get_cmap('gray'))
  
+
 F = np.absolute(Fx) + np.absolute(Fy)
-
-
 
 
 D = np.arctan2(Fx,Fy)
 D = np.degrees(D)
+#plt.imshow(D, cmap = plt.get_cmap('gray')); plt.show()
 
-plt.imshow(D, cmap = plt.get_cmap('gray')); plt.show()
+###########################
+##Nonmaximum suppression###
+###########################
 
-#########################
-#######QUESTION 2#########
-#########################
-
-
+##1. For each pixel find the direction D* in (0, 45, 90, 135) that is closest to the orientation D at that pixel.
 degList = [0,45,90,135]
 
-
-newD = [[0 for x in range(len(D[0]))] for y in range(len(D))]
-newD = np.array(newD)
-newD.shape
+D_star = [[0 for x in range(len(D[0]))] for y in range(len(D))]
+D_star = np.array(D_star)
+D_star.shape
 
 for x in range(D.shape[0]):
 	for y in range(D.shape[1]):
-		#print(D[x][y])
 		if D[x][y] < 0:
-			D[x][y] + 180
-		m = min(degList, key=lambda x:abs(x-D[x][y]))
-		newD[x][y] = m
-		print m
+			D[x][y] = D[x][y] + 180
+		D_star[x][y] = round(D[x][y]/45.0)
+		D_star[x][y] = (D_star[x][y])*45.0
 
-#plt.imshow(newD, cmap = plt.get_cmap('gray'))
+#plt.imshow(D_star, cmap = plt.get_cmap('gray')); plt.show()
+
+##2. If the edge strength F(x,y) is smaller than at least one of its neighbors along D*, set I(x,y) = 0, else set I(x,y) = F(x,y)
+
+I = [[0 for x in range(len(F[0]))] for y in range(len(F))]
+for x in range(F.shape[0]):
+	for y in range(F.shape[1]):
+		if(D_star[x,y] == 0 or D_star[x,y] == 180):
+			try:
+				if(F[x][y] < F[x+1][y] or F[x][y] < F[x-1][y]):
+					I[x][y] = 0
+				else:
+					I[x][y] = F[x][y]
+			except:
+				pass
+		elif(D_star[x,y] == 45):
+			try:
+				if(F[x][y] < F[x+1][y] or F[x][y] < F[x-1][y-1]):
+					I[x][y] = 0
+				else:
+					I[x][y] = F[x][y]
+			except:
+				pass
+		elif(D_star[x,y] == 90):
+			try:
+				if(F[x][y] < F[x][y+1] or F[x][y] < F[x][y-1]):
+					I[x][y] = 0
+				else:
+					I[x][y] = F[x][y]
+			except:
+				pass
+		else:
+			try:
+				if(F[x][y] < F[x-1][y+1] or F[x][y] < F[x+1][y-1]):
+					I[x][y] = 0.0
+				else:
+					I[x][y] = F[x][y]
+			except:
+				pass
+
+plt.imshow(I, cmap = plt.get_cmap('gray')); plt.show()
+
 
 
 
